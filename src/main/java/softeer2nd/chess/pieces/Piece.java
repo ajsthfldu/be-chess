8984a9f6ea3f6ac715d3e574930f404cbfbdab7e
ajsthfldu <ajsthfldu@outlook.com>
2023-07-05 17:58:35 +0900

    small edit

--- src/main/java/softeer2nd/chess/pieces/Piece.java
+++ src/main/java/softeer2nd/chess/pieces/Piece.java
@@ -9,13 +9,13 @@ public class Piece {
     }
 
     public enum Type {
-        PAWN('p', 1),
+        PAWN('p', 1.0),
         KNIGHT('n', 2.5),
-        BISHOP('b', 3),
-        ROOK('r', 5),
-        QUEEN('q', 9),
-        KING('k', 0),
-        NO_PIECE('.', 0);
+        BISHOP('b', 3.0),
+        ROOK('r', 5.0),
+        QUEEN('q', 9.0),
+        KING('k', 0.0),
+        NO_PIECE('.', 0.0);
 
         private final char representation;
         private final double defaultScore;

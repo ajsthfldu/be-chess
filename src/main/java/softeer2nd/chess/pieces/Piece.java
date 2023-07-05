6d00f00e9b321b6c1ce2775ad4b5c1cb1d70c534
ajsthfldu <ajsthfldu@outlook.com>
2023-07-05 11:49:45 +0900

    use enum in factory method

--- src/main/java/softeer2nd/chess/pieces/Piece.java
+++ src/main/java/softeer2nd/chess/pieces/Piece.java
@@ -27,105 +27,74 @@ public class Piece {
     }
 
 
-    public static final String BLACK_COLOR = "black";
-    public static final String WHITE_COLOR = "white";
-    public static final String EMPTY_COLOR = "empty";
-
-    public static final char EMPTY_REPRESENTATION = '.';
-
-    public static final char WHITE_PAWN_REPRESENTATION = 'p';
-    public static final char BLACK_PAWN_REPRESENTATION = 'P';
-
-    public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
-    public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
-
-    public static final char WHITE_ROOK_REPRESENTATION = 'r';
-    public static final char BLACK_ROOK_REPRESENTATION = 'R';
-
-    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
-    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
-
-    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
-    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
-
-    public static final char WHITE_KING_REPRESENTATION = 'k';
-    public static final char BLACK_KING_REPRESENTATION = 'K';
-
-
     public static Piece createBlank() {
-        return new Piece(EMPTY_COLOR, "empty", EMPTY_REPRESENTATION);
+        return new Piece(Color.NOCOLOR, Type.NO_PIECE);
     }
 
     public static Piece createWhitePawn() {
-        return new Piece(WHITE_COLOR, "pawn", WHITE_PAWN_REPRESENTATION);
+        return new Piece(Color.WHITE, Type.PAWN);
     }
     public static Piece createBlackPawn() {
-        return new Piece(BLACK_COLOR, "pawn", BLACK_PAWN_REPRESENTATION);
+        return new Piece(Color.BLACK, Type.PAWN);
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(WHITE_COLOR, "knight", WHITE_KNIGHT_REPRESENTATION);
+        return new Piece(Color.WHITE, Type.KNIGHT);
     }
     public static Piece createBlackKnight() {
-        return new Piece(BLACK_COLOR, "knight", BLACK_KNIGHT_REPRESENTATION);
+        return new Piece(Color.BLACK, Type.KNIGHT);
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(WHITE_COLOR, "rook", WHITE_ROOK_REPRESENTATION);
+        return new Piece(Color.WHITE, Type.ROOK);
     }
     public static Piece createBlackRook() {
-        return new Piece(BLACK_COLOR, "rook", BLACK_ROOK_REPRESENTATION);
+        return new Piece(Color.BLACK, Type.ROOK);
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(WHITE_COLOR, "bishop", WHITE_BISHOP_REPRESENTATION);
+        return new Piece(Color.WHITE, Type.BISHOP);
     }
     public static Piece createBlackBishop() {
-        return new Piece(BLACK_COLOR, "bishop", BLACK_BISHOP_REPRESENTATION);
+        return new Piece(Color.BLACK, Type.BISHOP);
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(WHITE_COLOR, "queen", WHITE_QUEEN_REPRESENTATION);
+        return new Piece(Color.WHITE, Type.QUEEN);
     }
     public static Piece createBlackQueen() {
-        return new Piece(BLACK_COLOR, "queen", BLACK_QUEEN_REPRESENTATION);
+        return new Piece(Color.BLACK, Type.QUEEN);
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(WHITE_COLOR, "king", WHITE_KING_REPRESENTATION);
+        return new Piece(Color.WHITE, Type.KING);
     }
     public static Piece createBlackKing() {
-        return new Piece(BLACK_COLOR, "king", BLACK_KING_REPRESENTATION);
+        return new Piece(Color.BLACK, Type.KING);
     }
 
-    private String color;
-    private String name;
-    private char representation;
+    private final Color color;
+    private final Type type;
 
-    private Piece(String color, String name, char representation) {
+    private Piece(Color color, Type type) {
         this.color = color;
-        this.name = name;
-        this.representation = representation;
+        this.type = type;
     }
 
-    public String getColor() {
+    public Color getColor() {
         return color;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public char getRepresentation() {
-        return representation;
+    public Type getType() {
+        return type;
     }
 
     public boolean isWhite() {
-        return WHITE_COLOR.equals(color);
+        return color == Color.WHITE;
     }
 
     public boolean isBlack() {
-        return BLACK_COLOR.equals(color);
+        return color == Color.BLACK;
     }
 
     @Override
@@ -136,15 +105,13 @@ public class Piece {
             return false;
         } else {
             Piece piece = (Piece) obj;
-            return this.name.equals(piece.name) &&
-                    this.color.equals(piece.color) &&
-                    this.representation == piece.representation;
+            return this.color == piece.color && this.type == piece.type;
         }
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(color, name, representation);
+        return Objects.hash(color, type);
     }
 
 }
--- src/test/java/softeer2nd/chess/pieces/PieceTest.java
+++ src/test/java/softeer2nd/chess/pieces/PieceTest.java
@@ -2,30 +2,24 @@ package softeer2nd.chess.pieces;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static softeer2nd.chess.pieces.Piece.*;
 
 public class PieceTest {
     @Test
     @DisplayName("기물은 올바른 색상과 표현방법을 가지고 생성되어야 한다")
     public void create_piece() {
-        verifyPiece(createWhitePawn(), WHITE_COLOR, WHITE_PAWN_REPRESENTATION);
-        verifyPiece(createBlackPawn(), BLACK_COLOR, BLACK_PAWN_REPRESENTATION);
+        verifyPiece(createWhitePawn(), createBlackPawn(), Type.PAWN);
+        verifyPiece(createWhiteKnight(), createBlackKnight(), Type.KNIGHT);
+        verifyPiece(createWhiteRook(), createBlackRook(), Type.ROOK);
+        verifyPiece(createWhiteBishop(), createBlackBishop(), Type.BISHOP);
+        verifyPiece(createWhiteQueen(), createBlackQueen(), Type.QUEEN);
+        verifyPiece(createWhiteKing(), createBlackKing(), Type.KING);
 
-        verifyPiece(createWhiteKnight(), WHITE_COLOR, WHITE_KNIGHT_REPRESENTATION);
-        verifyPiece(createBlackKnight(), BLACK_COLOR, BLACK_KNIGHT_REPRESENTATION);
-
-        verifyPiece(createWhiteRook(), WHITE_COLOR, WHITE_ROOK_REPRESENTATION);
-        verifyPiece(createBlackRook(), BLACK_COLOR, BLACK_ROOK_REPRESENTATION);
-
-        verifyPiece(createWhiteBishop(), WHITE_COLOR, WHITE_BISHOP_REPRESENTATION);
-        verifyPiece(createBlackBishop(), BLACK_COLOR, BLACK_BISHOP_REPRESENTATION);
-
-        verifyPiece(createWhiteQueen(), WHITE_COLOR, WHITE_QUEEN_REPRESENTATION);
-        verifyPiece(createBlackQueen(), BLACK_COLOR, BLACK_QUEEN_REPRESENTATION);
-
-        verifyPiece(createWhiteKing(), WHITE_COLOR, WHITE_KING_REPRESENTATION);
-        verifyPiece(createBlackKing(), BLACK_COLOR, BLACK_KING_REPRESENTATION);
+        Piece blank = createBlank();
+        assertFalse(blank.isWhite());
+        assertFalse(blank.isBlack());
+        assertEquals(Type.NO_PIECE, blank.getType());
     }
 
     @Test
@@ -34,11 +28,12 @@ public class PieceTest {
         assertEquals('P', Type.PAWN.getBlackRepresentation());
     }
 
-    private void verifyPiece(final Piece piece, final String color, final char representation) {
-        assertEquals(color, piece.getColor());
-        assertEquals(representation, piece.getRepresentation());
-        assertEquals(color.equals(WHITE_COLOR), piece.isWhite());
-        assertEquals(color.equals(BLACK_COLOR), piece.isBlack());
+    private void verifyPiece(final Piece whitePiece, final Piece blackPiece, final Type type) {
+        assertTrue(whitePiece.isWhite());
+        assertEquals(type, whitePiece.getType());
+
+        assertTrue(blackPiece.isBlack());
+        assertEquals(type, blackPiece.getType());
     }
 }
 
